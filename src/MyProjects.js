2e53f5b2e7bca9fd3b4169f6e6f1be16ecf27aa6
Gintare <37443286+gintarios@users.noreply.github.com>
2018-12-14 15:30:12 +0000

    links added

--- src/MyProjects.js
+++ src/MyProjects.js
@@ -7,9 +7,24 @@ const MyProjects = () => {
       <h2>My Projects</h2>
 
       <div className="projects-box">
-        <div className="project project1">Coming soon</div>
-        <div className="project project2">Coming Soon</div>
-        <div className="project project3">Coming Soon</div>
+        <div className="project project1">
+          <a href="https://ginflix.herokuapp.com/" target="_black">
+            {" "}
+            Ginflix App
+          </a>
+        </div>
+        <div className="project project2">
+          <a href="http://spotify-siftr.herokuapp.com/" target="_black">
+            {" "}
+            Softify Siftr
+          </a>
+        </div>
+        <div className="project project3">
+          <a href="" target="_black">
+            {" "}
+            Comming Soon...{" "}
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/Skills.js
+++ src/Skills.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
+import "./Skills.css";
 
 const Skills = () => {
   return (
     <div className="section-skills" id="skills">
       <h2>Skills</h2>
+
+      {/* <div className="skills-container">
+        <div className="skills-item">1</div>
+        <div className="skills-item">2</div>
+        <div className="skills-item">3</div>
+        <div className="skills-item">4</div>
+        <div className="skills-item">5</div>
+        <div className="skills-item">6</div>
+      </div> */}
     </div>
   );
 };
